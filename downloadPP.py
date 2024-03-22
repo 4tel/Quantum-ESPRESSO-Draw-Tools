bbd8d4df9691af455efe22d5f76092457682f283
4tel <62090735+4tel@users.noreply.github.com>
2024-03-22 11:11:00 +0900

    Update and rename downloadPS.py to downloadPP.py

--- downloadPP.py
+++ downloadPP.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
 from re import compile, findall
 from pathlib import Path
+from functools import wraps
 
 # Path
 QEPath = 'https://pseudopotentials.quantum-espresso.org'
@@ -16,12 +17,12 @@ atoms_pattern = compile('[A-Z][a-z]*')
 filelink_pattern = compile('["][/].+UPF["]')
 
 
-def get_html(url):
+def _get_html(url):
     response = urlopen(url)
     return response.read()
 
-def get_all_atoms(url):
-    html = get_html(url)
+def _get_all_atoms(url):
+    html = _get_html(url)
     atoms = []
     lines = html.split(b'\n')
     for line in lines:
@@ -29,8 +30,8 @@ def get_all_atoms(url):
             atoms.append(findall(atoms_pattern, str(line))[0])
     return atoms
 
-def get_file_links(url):
-    html = get_html(url)
+def _get_file_links(url):
+    html = _get_html(url)
     lines = html.split(b'\n')
     links = []
     for line in lines:
@@ -38,37 +39,62 @@ def get_file_links(url):
             links.append(findall(filelink_pattern, str(line))[0])
     return links
 
-def get_file(url):
-    html = get_html(url)
+def _get_file(url):
+    html = _get_html(url)
     return str(html, 'utf-8')
 
-if __name__ == '__main__':
+def timer(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        from time import time
+        start_time = time()  # 시작 시간 측정
+        result = func(*args, **kwargs)  # 원래 함수 실행
+        end_time = time()  # 종료 시간 측정
+        print(f"{func.__name__} 함수 실행 시간: {end_time - start_time}초")
+        return result
+    return wrapper
+
+@timer
+def main():
     curdir = Path(__file__).parent
 
+    print("Download Start")
     for PP in PP_Table:
+        print(f'{PP} Pseudo Potential Downloading..')
+
         url = f"{QEPath}/{TablePath}/{PP}"
+        atoms = _get_all_atoms(url)
         PPDir = curdir / PP
         PPDir.mkdir(exist_ok=True)
         
-        atoms = get_all_atoms(url)
         t = open(curdir/f'{PP}_log.txt','w')
+        t.write(f'all atoms count in {PP} : {len(atoms)}')
         for atom in atoms:
+            print(f'{atom} Downloading..')
+            t.write(atom)
+
             directory = PPDir / atom
             directory.mkdir(exist_ok=True)
-            t.write(atom+'\n')
-
             url = f'{QEPath}/{TablePath}/{PP}/{atom.lower()}'
-            files = get_file_links(url)
+            files = _get_file_links(url)
 
+            t.write(f'(count : {len(files)})\n')
             for file in files:
                 file = file.strip('"')
                 url = f'{QEPath}/{file}'
                 filename = file.split('/')[-1]
-
                 newFile = directory / filename
-                t.write(filename+'\n')
-                t.flush()
-                if not newFile.exists():
+
+                t.write(filename)
+                if newFile.exists():
+                    t.write(f'{filename} already exists')
+                else:
                     with open(newFile, 'w') as f:
-                        f.write(get_file(url))
+                        f.write(_get_file(url))
+                    t.write(f'{filename} successfully created')
+                t.flush()
             t.write('\n\n\n\n\n')
+    print('All Download Completed')
+
+if __name__ == '__main__':
+    main()
